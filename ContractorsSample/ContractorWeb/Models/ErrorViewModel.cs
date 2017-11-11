87e99083de3037328bc100fc4d61c5f3dbab86a4
k.godlewski94 <k.godlewski94@gmail.com>
2017-11-11 12:02:23 +0100

    Add sample project

---
+++ ContractorsSample/ContractorWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContractorWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
